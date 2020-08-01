82c68f92084ed3f75f8600a3434c4594748b553e
Dane Sirois <dane_sirois@live.com>
2020-08-01 11:55:39 -0700

    v3.0.0 Release (#31)

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Redux-Interfaces
-> Redux State-Management API builder:
+> Domain based Redux architecture and API.
 
-Interfaces define the consumption methods surrounding both the modification and retrieval of application state.
-With them the aim is to reduce boilerplate, while providing cross-repo code reuse/liberation.
+Interfaces are wrappers around your Redux actions and reducers that provide an importable API for interacting with your applications state. Interfaces are organized by domain allowing for easier conceptualization of your state. Through the library you can access and subscribe to data directly from your reducers without the need for external bindings. Dispatching actions can be done from anywhere and becomes as simple as a function call.
 
 **Why interfaces**:
 
-- **Liberate your state** - Interfaces give you access to your redux state from anywhere within your application. Dispatching actions is now just a function call, so bindings like `mapDispatchToProps()` are no longer needed.
-- **Write *once* then *reuse*** - Interfaces exist independently from the rest of your application. They even exist independently from other interfaces. Reuse your interface code across repos.
-- **Maintainability** - The nature of interfaces make you decide upon groupings for dividing up your applications state. You can think of it as organizing by *feature*, or *functionality*, opposed to by *type*. This division keep your state sane, while ultimately painting a piece of the broader picture of your applications state as a whole.
+- They reduce the amount of boilerplate needed for interacting with your store
+  - Say goodbye to bindings like `mapDistpatchToProps()`
+- They force you to think about the different domains in your app and model your state around them
+- They provide structure around your usage of Redux by giving you an easily repeatable and understandable architecture for adding new actions and reducers
 
 **Consumption**:
 
@@ -18,44 +18,40 @@ import { RI } from 'npm-redux-interfaces';
 
 // DISPATCHING ACTIONS:
 RI.app.LOADING(true);
-RI.user.ADD_FRIEND({ name: 'Don' });
 
-// Follow CRUD:
-RI.post.CREATE({...});
-RI.post.READ({...});
-RI.post.UPDATE({...});
-RI.post.DELETE(...);
+// Get the state from the loading reducer:
+const appIsLoading = RI.app.loading.getState();
 
-// ACCESSING REDUCERS:
-const posts = RI.posts.allPosts().getState();
+// Create a listener on the messages reducer:
+RI.chatroom.messages.listen((changes) => {
+  // Called on every change to the reducer
+});
 
-// Using React:
-function mapStateToProps() {
-  return ({
-    loading: RI.app.loading().getState(),
-    friendsList: RI.user.friendsList().getState()
-  });
-};
+// Add a message to a chatroom:
+RI.chatroom.NEW_MESSAGE({ text: 'My message' });
 ```
-
-- **Note:**
-This library may **not** currently support server-side rendering.. It's something that needs looking into.
+- (*See [#Creating-an-interface](#creating-an-interface) for more information on how to configure*)
 
 ## Index:
 1. [Installation:](#installation)
-2. [Usage:](#usage)
-3. [Configuration:](#configuration)
-4. [Creating an Interface:](#creating-an-interface)
-5. [API:](#api)
-6. [Dependencies:](#dependencies)
-7. [Author:](#author)
-8. [License:](#license)
+2. [Mirgrating to v3.0.0:](#Migrating-from-v2.0-to-v3.0)
+3. [Usage:](#usage)
+4. [Configuration:](#configuration)
+5. [Creating an Interface:](#creating-an-interface)
+6. [API:](#api)
+7. [Dependencies:](#dependencies)
+8. [Author:](#author)
+9. [License:](#license)
 
 ***
 ## Installation:
 ```
 npm install --save npm-redux-interfaces
 ```
+
+***
+## Migrating from v2.0 to v3.0:
+- see the [release notes](https://github.com/DaneSirois/npm-redux-interfaces/releases/tag/v3.0.0) for changes
 
 ***
 ## Usage:
@@ -78,6 +74,7 @@ export default {
   }
 };
 ```
+- (*See [#Creating-an-interface](#creating-an-interface) for more information on how to configure*)
 
 **Connecting an interface**:
 ```js
@@ -87,7 +84,7 @@ import { RI } from 'npm-redux-interfaces';
 import App_interface from './App/App_index';
 
 // Mount your interfaces:
-RI.mountInterface('app', App_interface);
+RI.mount('chatroom', Chatroom_interface);
 
 // Export the root_reducer:
 export const root_reducer = RI.getRootReducer();
@@ -97,14 +94,18 @@ export const root_reducer = RI.getRootReducer();
 ```js
 import { RI } from 'npm-redux-interfaces';
 
-RI.app.RENDER(true);
+RI.chatroom.NEW_MESSAGE({ text: 'Cool!' });
 ```
 
-**Accessing a reducer:**
+**Accessing state from reducers:**
 ```js
 import { RI } from 'npm-redux-interfaces';
 
-const renderApp = RI.app.render().getState(); // true
+const messages = RI.chatroom.messages.getState();
+
+RI.chatroom.messages.listen((changes) => {
+  // Called on every state change in the reducer
+});
 ```
 
 ***
@@ -115,7 +116,7 @@ const renderApp = RI.app.render().getState(); // true
 
 **[2]**: Create an `index.js` file within your new *interfaces* folder. This file is where you will make the connection between your interfaces, and the library. At the end of this file, export the `root_reducer`.
 
-**[interfaces/index.js]**:
+**[ src/interfaces/index.js ]:**
 ```js
 import { RI } from 'npm-redux-interfaces';
 
@@ -123,7 +124,7 @@ import { RI } from 'npm-redux-interfaces';
 import Chatroom_interface from './Chatroom/Chatroom_index';
 
 // Mount your interfaces:
-RI.mountInterface('chatroom', Chatroom_interface);
+RI.mount('chatroom', Chatroom_interface);
 
 // Export the root_reducer:
 export const root_reducer = RI.getRootReducer();
@@ -131,7 +132,7 @@ export const root_reducer = RI.getRootReducer();
 
 **[3]**: Navigate to your apps root `index.js` file and create the store. Immediately after definition, pass in reference to it with `RI.setStore()`.
 
-**[/index.js]:**
+**[ src/index.js ]:**
 ```js
 import { RI } from 'npm-redux-interfaces';
 import { root_reducer } from './interfaces/index.js';
@@ -150,62 +151,38 @@ RI.setStore(store);
 **[1]**: Create a new folder for your interface inside of the `/interfaces` directory:
 - It's convention to capitalize the first letter of the interface folders name (**ex**: `Chatroom`).
 
-**[2]**: Inside of the new folder, create a **types** file. This is where your will define your Redux types:
-- It's convention to name your types with capitals.
-- You do not need a *types* file if you are using [`type-stash`](https://www.npmjs.com/package/type-stash)(recommended).
+[2]: Inside of this new folder, create a **types** file:
+- *It's convention to name your constants with the name of the interface appended with the type in all capitals.*
 
-**[/interfaces/App/App_types.js]**:
+**[ src/interfaces/Chatroom/Chatroom_types.js ]:**
 ```js
-export const type__APP_RENDER = 'type__APP_RENDER';
+export const Chatroom_NEW_MESSAGE = 'Chatroom_NEW_MESSAGE';
 ```
 
 **[3]**: Create sub directories for your *actions* and *reducers*:
 
-**[/interfaces/App/actions/App_RENDER.js]:**
+**[ src/interfaces/Chatroom/actions/Chatroom_NEW_MESSAGE.js ]:**
 ```js
-// Action:
-import { type__APP_RENDER } from '../App_types.js';
+import { Chatroom_NEW_MESSAGE } from '../Chatroom_types.js';
 
-export default (bool) => {
+export default (message) => {
   return {
-    type: type__APP_RENDER,
-    payload: bool
-  };
-};
-
-// With type-stash:
-import typeStash from 'type-stash';
-
-export default (bool) => {
-  return {
-    type: typeStash.use('APP_RENDER'),
-    payload: bool
+    type: Chatroom_NEW_MESSAGE,
+    payload: {
+      message
+    }
   };
 };
 ```
 
-**[/interfaces/App/reducers/App_render.js]:**
+**[ src/interfaces/Chatroom/reducers/Chatroom_messages.js ]:**
 ```js
-// Reducer:
-import { type__App_RENDER } from '../App_types.js';
+import { Chatroom_NEW_MESSAGE } from '../Chatroom_types.js';
 
 export default (state = false, action) => {
   switch(action.type) {
-    case type__App_RENDER:
-      return action.payload;
-    default:
-      return state;
-  };
-};
-
-// With type-stash:
-import typeStash from 'type-stash';
-
-export default (state = false, action) => {
-  switch(action.type) {
-    case typeStash.use('App_RENDER'):
-      return action.payload;
-    break;
+    case Chatroom_NEW_MESSAGE:
+      return state.concat([action.payload]);
     default:
       return state;
   };
@@ -214,10 +191,10 @@ export default (state = false, action) => {
 
 **[4]**: Create the entry point for your interface. This is where you build and expose it's public API.
 
-- This file **must** export an object that contains the properties *actions*, and *reducers*. It's okay to omit either key, but do know that their presence is required if you wish to use that part of the interface.
-- Your actions **must** be named in all caps. This is what differentiates them from your reducers.
+- *This file **must** export an object that contains the keys **actions**, and **reducers**. It's okay to omit either key, but know that their presence is required if you wish to use that part of your interface.*
+- *Your actions **must** be named in all caps. This is what differentiates them from your reducers.*
 
-**[/interfaces/App/App_index.js]:**
+**[ src/interfaces/Chatroom/Chatroom_index.js ]:**
 ```js
 import App_RENDER from './actions/App_RENDER.js';
 
@@ -239,9 +216,7 @@ export default {
 
 ***
 ## API:
-## RI.mountInterface([*string*], [*object*]):
-**Note**: This is being renamed to `RI.mount()` in **v3**.
-
+## RI.mount([*string*], [*object*]):
 This method mounts your interface into to the library giving you access to it's internal API.
 - As convention, namespace your interfaces using lowercase.
 
@@ -256,16 +231,10 @@ This method mounts your interface into to the library giving you access to it's 
 
 **Example**:
 ```js
-RI.mountInterface('app', App_interface);
+RI.mount('chatroom', Chatroom_interface);
 ```
 
 ***
-## RI.connectInterface([*string*], [*object*]):
-- **This method is being replaced by `RI.mount()` in v3.**
-
-This still works for now, but it will be removed in version 3.
-***
-
 ## RI.getRootReducer():
 This method returns the `root_reducer` of your app.
 - Usually you will want to immediately export this for use when defining your store.
--- src/index.js
+++ src/index.js
@@ -1,29 +1,247 @@
 import { combineReducers } from 'redux';
 
-// ====== Private ====== //
+const Store = ((initial = null) => {
+  // Set the initial store passed in from the user:
+  let ClientStore = initial;
 
-const _Store = ((initial = null) => {
-  let storeInstance = initial;
+  /**
+   * createReducerListener
+   *
+   * A method for creating a reducer listener and returning
+   * an API to be used to add new listner hooks and to update
+   * the existing ones with new state.
+   */
+  const createReducerListener = (interfaceName, reducerName, callback) => {
+    let currentValueInReducer = null;
 
-  return {
+    /**
+    * hooks
+    *
+    * An IIFE that is responsible for keeping track
+    * of every listener hook. Listener hooks are
+    * instantiated through the `.listen` command.
+    */
+    const hooks = (() => {
+      const instances = [];
+
+      return {
+        getAll: () => (
+          instances
+        ),
+        add: (hook) => {
+          instances.push(hook);
+        },
+        remove: (position) => {
+          instances[position] = null;
+        },
+        length: instances.length
+      };
+    })();
+
+    return {
+      [reducerName]: {
+        updateListeners: (newState) => {
+          const updateHooks = () => {
+            currentValueInReducer = nextReducerState;
+
+            // Update every listener hook with the new value:
+            hooks.getAll().forEach((listener) => {
+              if (listener) {
+                listener.callback(nextReducerState);
+              }
+            });
+          };
+
+          const nextReducerState = newState[interfaceName][reducerName];
+
+          const dataTypeOfNextState = typeof nextReducerState;
+          const dataTypeOfCurrentState = typeof currentValueInReducer;
+
+          /**
+           * JSON.stringify works here because the order of
+           * arrays or objects should not change by Redux. If it
+           * does change then we want it to update the hooks.
+           */
+          if (
+            dataTypeOfNextState !== dataTypeOfCurrentState ||
+            JSON.stringify(nextReducerState) !== JSON.stringify(currentValueInReducer)
+          ) {
+            // If the data has changed we need to update:
+            updateHooks();
+          }
+        },
+        addListenerHook: (callback) => {
+          /**
+           * addListenerHook
+           *
+           * The method is called when adding a new listener hook.
+           * the `callback` argument is what gets called internally
+           * to update the listener with the new state on change.
+           */
+
+          const listenerHook = {
+            remove: () => hooks.remove(hooks.length),
+            callback
+          };
+
+          hooks.add(listenerHook);
+
+          // populate with existing state:
+          callback(ClientStore.getState()[interfaceName][reducerName]);
+
+          return {
+            remove: listenerHook.remove
+          };
+        }
+      }
+    };
+  };
+
+  /**
+   * reducerSubscriptions
+   *
+   * An IIFE that is responsible for instantiating and
+   * keeping track of reducer subscriptions. A given reducer
+   * can have only one subscription. Each subscription can
+   * having multiple listener hooks.
+   */
+  const reducerSubscriptions = (() => {
+    const subscriptions = {};
+
+    return {
+      listen: (interfaceName, reducerName, callback) => {
+        /**
+         * This function is executed in the context of a
+         * new listener being added to a reducer subscription.
+         */
+
+        let publicListenerHookMethods = null;
+
+        if (!subscriptions.hasOwnProperty(interfaceName)) {
+          // if the interface does not yet have any reducer
+          // subscriptions, instantiate the first one:
+          subscriptions[interfaceName] = createReducerListener(
+            interfaceName,
+            reducerName,
+            callback
+          );
+
+          publicListenerHookMethods = (
+            subscriptions[interfaceName][reducerName].addListenerHook(callback)
+          );
+        } else if (
+          subscriptions.hasOwnProperty(interfaceName) &&
+          !subscriptions[interfaceName][reducerName]
+        ) {
+          // If the interface already has a subscription but
+          // no listeners for this reducer, add the reducer
+          // listener to the interface subscription:
+          subscriptions[interfaceName] = Object.assign({},
+            subscriptions[interfaceName],
+            createReducerListener(
+              interfaceName,
+              reducerName,
+              callback
+            )
+          );
+
+          publicListenerHookMethods = (
+            subscriptions[interfaceName][reducerName].addListenerHook(callback)
+          );
+        } else {
+          // if the interface already has a subscription and
+          // the reducer already has a listener, add a new
+          // listener hook:
+          publicListenerHookMethods = (
+            subscriptions[interfaceName][reducerName].addListenerHook(callback)
+          )
+        }
+
+        return publicListenerHookMethods;
+      },
+      updateSubscriptions: (changesFromStore) => {
+        /**
+         * An internal method for updating all reducer subscriptions
+         * with the new state object.
+         *
+         * This is called internally on every state change in the store.
+         */
+        for (let namespace in subscriptions) {
+          const Interface = subscriptions[namespace];
+
+          for (let reducerName in Interface) {
+            Interface[reducerName].updateListeners(changesFromStore);
+          }
+        }
+      }
+    };
+  })();
+
+  const subscribe = () => {
+    /**
+     * subscribe
+     *
+     * A method to be called on every update made to the
+     * store. It is responsible for updating every reducer
+     * subscription with the new state.
+     *
+     * @return {null}
+     */
+
+    /**
+     * handleChange
+     *
+     * A method for updating all of the subscriptions
+     * with the new state from Redux:
+     */
+    const handleChange = () => {
+      const nextState = ClientStore.getState();
+
+      reducerSubscriptions.updateSubscriptions(nextState);
+    };
+
+    // Subscribe to the store passed from the user
+    // and save the disconnect method:
+    let unsubscribeCallback;
+    const createSubscriptionToStore = (store) => {
+      // returns method to unsubscribe;
+      unsubscribeCallback = store.subscribe(handleChange);
+    };
+
+    return Object.assign({}, reducerSubscriptions, {
+      createSubscriptionToStore,
+      unsubscribeCallback
+    });
+  };
+
+  const StoreAPI = {
+    subscribe: subscribe(),
     dispatch: (action) => {
-      return storeInstance.dispatch(action);
+      ClientStore.dispatch(action);
     },
     get: () => {
-      return storeInstance;
+      return ClientStore;
     },
     set: (store) => {
-      return storeInstance = store;
+      ClientStore = store;
+      StoreAPI.subscribe.createSubscriptionToStore(store);
     }
-  }
+  };
+
+  return StoreAPI;
 })();
 
-const _Reducers = ((initial = null) => {
+const Reducer = (store, interfaceName, reducerName) => ({
+  getState: () => eval(`store.get().getState().${interfaceName}.${reducerName}`),
+  listen: (callback) => store.subscribe.listen(interfaceName, reducerName, callback)
+});
+
+const RootReducer = ((initial = null) => {
   let root_reducer = initial;
   let reducerStore = {};
 
   return {
-    add: (name, input) => {
+    addInterface: (name, input) => {
       // Format the inputs:
       const nextInterface = {[name]: input};
 
@@ -33,68 +251,62 @@ const _Reducers = ((initial = null) => {
       // Rebuild the root_reducer:
       return root_reducer = combineReducers(reducerStore);
     },
-    getRoot: () => {
+    get: () => {
       return root_reducer;
     }
   }
 })();
 
-// ====== Reducer Methods: ====== //
-const _getState = (interfaceName, reducer, Store) => eval(`Store.getState().${interfaceName}.${reducer}`);
+const mountInterface = (store, interfaceName, input) => {
+  // Check if the interface name does not conflict:
+  if (!RI[interfaceName]) {
 
-// ====== Interface Methods: ====== //
-const _mountInterface = (name, input) => {
-  if (!RI[name]) { // If the interface does not conflict:
-
+    // Build the actions:
     let actionsObj = {};
-    if (input.actions) { // Build the actions:
-      actionsObj = Object.keys(input.actions).reduce((obj, index) => {
-        obj[index] = (...payload) => _Store.dispatch(input.actions[index](...payload));
+    if (input.actions) {
+      actionsObj = Object.keys(input.actions).reduce((obj, action) => {
+        obj[action] = (...payload) => store.dispatch(input.actions[action](...payload));
         return obj;
       }, {});
     }
 
+    // Build the reducers:
     let reducersObj = {};
-    if (input.reducers) { // Build the reducers:
-      reducersObj = Object.keys(input.reducers).reduce((obj, index) => {
-        obj[index] = () => ({ getState: () => _getState(name, index, _Store.get()) });
-        return obj;
+    if (input.reducers) {
+      reducersObj = Object.keys(input.reducers).reduce((acc, reducerName) => {
+        acc[reducerName] = new Reducer(store, interfaceName, reducerName);
+        return acc;
       }, {});
 
-      // Mount the reducers:
-      _Reducers.add(name, combineReducers(input.reducers));
+      // Add the interface's reducers to the RootReducer:
+      RootReducer.addInterface(interfaceName, combineReducers(input.reducers));
     }
-
-    // Build the interface:
-    const nextInterface = { [name]: Object.assign(actionsObj, reducersObj) };
 
     // Mount the interface:
-    return RI = Object.assign(RI, nextInterface);
+    return RI = Object.assign({},
+      RI,
+      { [interfaceName]: Object.assign({}, actionsObj, reducersObj) }
+    );
   }
   // If the interface conflicts:
-  const err = { message: `Interface '${name}' is already in use. Try a different name.` };
-
-  console.log(err.message);
+  console.error(`
+    Interface '${interfaceName}' is already in use. Try a different name for your interface.
+  `);
 
   return err;
 };
 
-// ====== Public ====== //
-
 export let RI = {
-  connectInterface: (name, input) => {
-    return _mountInterface(name, input);
-  },
-  mountInterface: (name, input) => {
-    return _mountInterface(name, input);
+  mount: (namespace, input) => {
+    return mountInterface(Store, namespace, input);
   },
   setStore: (input) => {
-    return _Store.set(input);
+    return Store.set(input);
   },
   getStore: () => {
-    return _Store.get();
+    return Store.get();
   },
   getRootReducer: () => {
-    return _Reducers.getRoot();
+    return RootReducer.get();
   }
 };
